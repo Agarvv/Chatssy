7677ed372b585895a4414e9b6c52f906b4020890
Agarvv <casluagarv@gmail.com>
2024-11-29 13:57:51 +0100

    Config react router

---
+++ client/src/routes.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { LoginForm } from './features/auth/login/LoginForm'
+import { RegisterForm } from './features/auth/register/RegisterForm'
+
+export const AppRoutes = () => (
+  <Router>
+     <Routes>
+         <Route path="/register" element={ <RegisterForm /> }/> 
+         <Route path="/login" element={ <LoginForm />}/> 
+     </Routes>
+  </Router> 
+)
