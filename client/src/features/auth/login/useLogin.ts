a9f70b472103068f7e4c9434c461cf8557041d34
Agarvv <casluagarv@gmail.com>
2024-11-30 12:35:08 +0100

    implementing reset password frontend

--- client/src/features/auth/login/useLogin.ts
+++ client/src/features/auth/login/useLogin.ts
@@ -2,19 +2,29 @@ import { useMutation } from '@tanstack/react-query';
 import axiosInstance from 'src/config/axiosConfig'; 
 import { FormValues } from './types';
 
+import { useDispatch } from 'react-redux';
+import { setLoading, setSuccess, setError, clearMessages } from 'src/store/apiStatus/apiStatusSlice';
+import { AppDispatch } from 'src/store/apiStatus/apiStatusStore';
+
 const loginUser = async (data: FormValues): Promise<any> => {
   const response = await axiosInstance.post('/auth/login', data);
   return response.data;
 };
 
 export const useLogin = () => {
+  const dispatch: AppDispatch = useDispatch(); 
   return useMutation({
     mutationFn: (data: FormValues) => loginUser(data),
+    onMutate: () => {
+        dispatch(setLoading()); 
+    }, 
     onSuccess: (data: any) => {
       console.log('Welcome back!:', data);
+      dispatch(setSuccess('Welcome Back!'))
     },
     onError: (error: Error) => {
       console.error('Something went wrong..:', error);
+      dispatch(setError('Something went wrong...'))
     }
   });
 };
