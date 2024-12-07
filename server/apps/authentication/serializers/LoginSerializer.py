14db917b804dda4da7d09d7d1f948b3af1a149a9
Agarvv <casluagarv@gmail.com>
2024-12-07 17:06:16 +0100

    issues

--- server/apps/authentication/serializers/LoginSerializer.py
+++ server/apps/authentication/serializers/LoginSerializer.py
@@ -1,20 +1,14 @@
-from rest_framework import serializers
-from django.core.exceptions import ValidationError
-from apps.user.models import User
-
 class LoginSerializer(serializers.ModelSerializer):
     class Meta: 
         model = User 
         fields = ['email', 'password']
-        
+
     def validate_email(self, value):
-        if not value:
-            raise ValidationError('Email Is Required')
-        if not User.objects.filter(email = value).exists():
-            raise ValidationError('That Email Does Not Exist.')
-        
-        return value 
-    
+        if not User.objects.filter(email=value).exists():
+            raise ValidationError('That Email Does Not Exist.') 
+        return value
+
     def validate_password(self, value):
         if not value:
             raise ValidationError('Password Is Required')
+        return value
