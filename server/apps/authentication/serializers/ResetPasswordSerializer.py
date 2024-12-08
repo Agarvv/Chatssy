636ebb5c1b75969d24a0b601a356e355c562067b
Agarvv <casluagarv@gmail.com>
2024-12-08 20:52:22 +0100

    issues

--- server/apps/authentication/serializers/ResetPasswordSerializer.py
+++ server/apps/authentication/serializers/ResetPasswordSerializer.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 from django.core.exceptions import ValidationError
-from apps.user.models import User
 
 class ResetPasswordSerializer(serializers.Serializer):
     email = serializers.EmailField()
     token = serializers.CharField()
-    password = serializers.CharField()
-    
-   
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        return data
