5824831c727f52097744c700946d33c0e4cbbc5a
Agarvv <casluagarv@gmail.com>
2024-12-08 13:11:50 +0100

    .env issues

--- client/src/config/axiosConfig.ts
+++ client/src/config/axiosConfig.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosInstance } from 'axios';
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: `${process.env.API_URL}/api/chatssy`,
+  baseURL: `${process.env.REACT_APP_API_URL}/api/chatssy`,
   timeout: 10000,  
 });
-//
+// .env 
 export default axiosInstance;
