ba594833c4478af9c5fa1115cfef264b5b080c22
Agarvv <casluagarv@gmail.com>
2024-12-10 13:47:46 +0100

    issues

--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -3,13 +3,14 @@ import React from 'react'
 import Header from 'src/layout/header/Header'
 import ContactsAside from 'src/layout/contacts-aside/ContactsAside'
 import Chatbox from 'src/features/chatbox/Chatbox'
+import styles from './HomePage.module.css'
 
 const HomePage = () => {
     return(
         <>
           <Header />
           
-          <div className="main-container">
+          <div className={styles.mainContainer}>
              <ContactsAside contacts={[]}/> 
              <Chatbox /> 
           </div>
