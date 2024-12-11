aa75dabdda957950a5338fc8d664e8aef5cb745a
Agarvv <casluagarv@gmail.com>
2024-12-11 01:16:11 +0100

    ui issue

--- client/src/layout/contacts-aside/aside-group/AsideGroup.tsx
+++ client/src/layout/contacts-aside/aside-group/AsideGroup.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
 import sharedContactStyles from '../aside-contact/AsideContact.module.css'
+import { logo } from 'src/logo.svg'
 
 const AsideGroup: React.FC = () => {
     return(
       <div className={`${sharedContactStyles.group} ${sharedContactStyles['aside-chat']}`}>
         <div className={sharedContactStyles['ac-user']}>
-          <img src="Screenshot_20240928-011835.png" alt="ñ" />
+          <img src={logo} alt="ñ" />
           <div className={sharedContactStyles['ac-content']}>
             <h4>Los Papus :P</h4>
             <p>Siii</p>
--- client/src/layout/contacts-aside/aside-user/AsideUser.tsx
+++ client/src/layout/contacts-aside/aside-user/AsideUser.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import sharedContactStyles from '../aside-contact/AsideContact.module.css'
+import { logo } from 'src/logo.svg'
 
 const AsideUser: React.FC = () => {
     return(
       <div className={`${sharedContactStyles.user} ${sharedContactStyles['aside-chat']}`}>
         <div className={sharedContactStyles['ac-user']}>
-          <img src="Screenshot_20240928-011835.png" alt="ñ" />
+          <img src={logo} alt="ñ" />
           <div className={sharedContactStyles['ac-content']}>
             <h4>Elver Galarga</h4>
             <p>Te hecho de m...</p>
