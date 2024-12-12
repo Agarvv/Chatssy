ab6a8e199103af927782376e27efb0064b858353
Agarvv <casluagarv@gmail.com>
2024-12-12 01:54:55 +0100

    slip up

--- server/apps/chat/serializers/StartChatSerializer.py
+++ server/apps/chat/serializers/StartChatSerializer.py
@@ -7,5 +7,5 @@ class StartChatSerializer(serializers.Serializer):
     receiver_id = serializers.IntegerField() 
     
     def validate(self, data):
-        if not self.receiver_id:
+        if not data.receiver_id:
             raise ValidationError('receiver id must be provided')
