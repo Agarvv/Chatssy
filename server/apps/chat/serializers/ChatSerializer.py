1428bcb41a813f34e14ff100fa85e905c7c9a616
Agarvv <casluagarv@gmail.com>
2024-12-13 00:24:47 +0100

    some issue

--- server/apps/chat/serializers/ChatSerializer.py
+++ server/apps/chat/serializers/ChatSerializer.py
@@ -1,9 +1,11 @@
-from rest_framework import serializers
 from django.core.exceptions import ValidationError
 from ..models import Chat
+from .MessageSerializer import MessageSerializer
+from rest_framework import serializers
 
-# full chat serializer, with messages
-class UserChatSerializer(serializers.ModelSerializer):
+# full chat serializer with messages
+class ChatSerializer(serializers.ModelSerializer):
+    messages = MessageSerializer(many=True)
     class Meta:
         model = Chat
-        fields = ('id', 'sender_id', 'receiver_id')
+        fields = ('id', 'sender_id', 'receiver_id', 'messages')
--- server/apps/chat/serializers/UserChatSerializer.py
+++ server/apps/chat/serializers/UserChatSerializer.py
@@ -1,10 +1,8 @@
 from rest_framework import serializers
 from django.core.exceptions import ValidationError
 from ..models import Chat
-from .MessageSerializer import MessageSerializer
 
 class UserChatSerializer(serializers.ModelSerializer):
-    messages = MessageSerializer(many=True)
     class Meta:
         model = Chat
-        fields = ('id', 'sender_id', 'receiver_id', 'messages')
+        fields = ('id', 'sender_id', 'receiver_id')
