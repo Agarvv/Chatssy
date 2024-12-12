cf4aa3311e2894c91f8809a8b932bb11aa3f2625
Agarvv <casluagarv@gmail.com>
2024-12-12 09:56:04 +0100

    issues

--- client/src/hooks/useContacts.ts
+++ client/src/hooks/useContacts.ts
@@ -4,20 +4,21 @@ import { setContacts } from 'src/store/chat/chatSlice';
 import { AppDispatch } from 'src/store/apiStatus/apiStatusStore';
 import { getUserContacts } from 'src/api/services/chat/ChatService';
 
- const useContacts = () => {
+const useContacts = () => {
     const dispatch: AppDispatch = useDispatch();
-    return useQuery({
-        queryKey: ['contacts'], 
-        queryFn: getUserContacts,
-        onSuccess: (data: any) => {
-            console.log('User contacts', data);
-            // dispatch(setContacts(data)); 
-        },
-        onError: (error: any) => {
-            console.error('oops', error);
-            dispatch(setError('Something went wrong while loading your contacts'))
-        },
-    });
-};
 
-export default useContacts;
+    return useQuery(
+        ['contacts'],
+        getUserContacts, 
+        {
+            onSuccess: (data) => {
+                console.log('User contacts', data);
+                dispatch(setContacts(data));
+            },
+            onError: (error) => {
+                console.error('Oops', error);
+                dispatch(setError('Something went wrong while loading your contacts'));
+            },
+        }
+    );
+};
