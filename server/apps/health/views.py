0997623bd555cdbc13e4f090db676d555f5e4208
Agarvv <casluagarv@gmail.com>
2024-12-13 00:34:23 +0100

    issues with viewsets

--- server/apps/health/views.py
+++ server/apps/health/views.py
@@ -2,7 +2,20 @@ from django.http import JsonResponse
 from django.http import request
 # i love Python <3
 
+from apps.util.AuthOutil import get_authenticated_user_id
+from apps.chat.chatService import handle_chat_creation, get_user_contacts
+from apps.chat.serializers.UserContactsSerializer import UserContactsSerializer
+
+#def check_server_health(request):
+#    return JsonResponse({
+#        "message": "Django Server Running!"
+#    })
+
 def check_server_health(request):
-    return JsonResponse({
-        "message": "Django Server Running!"
-    })
+       user_id = get_authenticated_user_id(request.COOKIES.get('jwt'))
+        
+        contacts = get_user_contacts(user_id)
+        
+        serializer = UserContactsSerializer(contacts)
+        
+        return Response({"contacts": contacts})
