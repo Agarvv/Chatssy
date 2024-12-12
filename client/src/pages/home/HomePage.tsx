0243725b4f47cbb2089eb70cd15887c06cd274de
Agarvv <casluagarv@gmail.com>
2024-12-12 18:28:03 +0100

    issues

--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -11,7 +11,7 @@ const HomePage = () => {
           <Header />
           
           <div className={styles.mainContainer}>
-                <ContactsAside contacts={[]}/> 
+                <ContactsAside/> 
 
                  <Chatbox /> 
           </div>
