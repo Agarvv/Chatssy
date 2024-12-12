3dd6776f75a0f7a4ef622c040d1081d4629b399c
Agarvv <casluagarv@gmail.com>
2024-12-13 00:39:39 +0100

    issues

--- server/apps/health/views.py
+++ server/apps/health/views.py
@@ -6,16 +6,11 @@ from apps.util.AuthOutil import get_authenticated_user_id
 from apps.chat.chatService import handle_chat_creation, get_user_contacts
 from apps.chat.serializers.UserContactsSerializer import UserContactsSerializer
 
-#def check_server_health(request):
-#    return JsonResponse({
-#        "message": "Django Server Running!"
-#    })
-
 def check_server_health(request):
-       user_id = get_authenticated_user_id(request.COOKIES.get('jwt'))
+    user_id = get_authenticated_user_id(request.COOKIES.get('jwt'))
         
-        contacts = get_user_contacts(user_id)
+    contacts = get_user_contacts(user_id)
         
-        serializer = UserContactsSerializer(contacts)
+    serializer = UserContactsSerializer(contacts)
         
-        return Response({"contacts": contacts})
+    return JsonResponse({"contacts": serializer.data})
