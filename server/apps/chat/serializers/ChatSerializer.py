061a62fb9c0d5ff305bf82ea49146d535e7b323a
Agarvv <casluagarv@gmail.com>
2024-12-13 14:02:03 +0100

    issues

--- server/apps/chat/serializers/ChatSerializer.py
+++ server/apps/chat/serializers/ChatSerializer.py
@@ -13,6 +13,6 @@ class ChatSerializer(serializers.ModelSerializer):
     def get_user_to_display_info(self, obj):
         user_id = self.context.get('user_id')
         
-        other_user = User.objects.get(id=obj.receiver_id) if obj.sender_id == user_id else User.objects.get(id=obj.sender_id)
+        other_user = User.objects.get(id=obj['receiver_id']) if obj['sender_id'] == user_id else User.objects.get(id=obj['sender_id'])
         
         return UserDetailsSerializer(other_user).data
