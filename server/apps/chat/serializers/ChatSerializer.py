a6a4d5d015c6ff941ee7d32fcf8d4e72b5cea627
Agarvv <casluagarv@gmail.com>
2024-12-13 13:49:18 +0100

    issue solved

--- server/apps/chat/serializers/ChatSerializer.py
+++ server/apps/chat/serializers/ChatSerializer.py
@@ -1,9 +1,3 @@
-from django.core.exceptions import ValidationError
-from ..models import Chat
-from .MessageSerializer import MessageSerializer
-from rest_framework import serializers
-from apps.user.serializers.UserDetailsSerializer import UserDetailsSerializer
-
 class ChatSerializer(serializers.ModelSerializer):
     messages = MessageSerializer(many=True)
     user_to_display_info = serializers.SerializerMethodField()
@@ -14,5 +8,7 @@ class ChatSerializer(serializers.ModelSerializer):
 
     def get_user_to_display_info(self, obj):
         user_id = self.context.get('user_id')
-        other_user = obj.receiver if obj.sender_id == user_id else obj.sender
+        
+        other_user = User.objects.get(id=obj.receiver_id) if obj.sender_id == user_id else User.objects.get(id=obj.sender_id)
+        
         return UserDetailsSerializer(other_user).data
