8cb4c3a5a2a1cea15792ce83afce36c81e1351ac
Agarvv <casluagarv@gmail.com>
2024-12-14 12:12:28 +0100

    issues

--- server/apps/chat/serializers/ChatSerializer.py
+++ server/apps/chat/serializers/ChatSerializer.py
@@ -11,6 +11,8 @@ class ChatSerializer(serializers.ModelSerializer):
         fields = ('id', 'sender_id', 'receiver_id', 'user_to_display_info', 'messages')
   
     def get_user_to_display_info(self, obj):
+        user_id = self.context.get('user_id')
+        
         if isinstance(obj, dict):
             sender_id = obj.get('sender_id') 
             receiver_id = obj.get('receiver_id')
@@ -18,10 +20,7 @@ class ChatSerializer(serializers.ModelSerializer):
             sender_id = obj.sender_id
             receiver_id = obj.receiver_id
 
-        if sender_id is not None and receiver_id is not None:
-            
-            user_id = self.context.get('user_id')
-            other_user_id = receiver_id if sender_id == user_id else sender_id
-            other_user = User.objects.get(id=other_user_id)
+        other_user_id = receiver_id if sender_id == user_id else sender_id
+        other_user = User.objects.get(id=other_user_id)
 
-            return UserDetailsSerializer(other_user).data
+        return UserDetailsSerializer(other_user).data
--- server/apps/health/views.py
+++ server/apps/health/views.py
@@ -2,15 +2,8 @@ from django.http import JsonResponse
 from django.http import request
 # i love Python <3
 
-from apps.util.AuthOutil import get_authenticated_user_id
-from apps.chat.chatService import handle_chat_creation, get_user_contacts
-from apps.chat.serializers.UserContactsSerializer import UserContactsSerializer
-
 def check_server_health(request):
-    user_id = get_authenticated_user_id(request.COOKIES.get('jwt'))
-        
-    contacts = get_user_contacts(user_id)
-        
-    serializer = UserContactsSerializer(contacts)
-        
-    return JsonResponse({"contacts": serializer.data})
+    return Response({
+        "message": "All Fine!"
+    })
+    
