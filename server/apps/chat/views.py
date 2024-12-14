bbac9b82d7fcbfa5f0a631dd5cd1c05f0e106eca
Agarvv <casluagarv@gmail.com>
2024-12-14 11:39:51 +0100

    fixing issues

--- server/apps/chat/views.py
+++ server/apps/chat/views.py
@@ -1,43 +1,31 @@
-from rest_framework import viewsets, status
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Chat
 from .serializers.StartChatSerializer import StartChatSerializer
 from .serializers.UserContactsSerializer import UserContactsSerializer
-from .serializers.ChatSerializer import ChatSerializer 
-
+from serializers.ChatSerializer import ChatSerializer 
 from apps.util.AuthOutil import get_authenticated_user_id
 from .chatService import handle_chat_creation, get_user_contacts
 
-class ChatViewSet(viewsets.ModelViewSet):
-    queryset = Chat.objects.all()
-
-    def create(self, request, *args, **kwargs):
+class ChatApiView(APIView):
+    # create user chat 
+    def post(self, request, *args, **kwargs):
         serializer = StartChatSerializer(data=request.data)
-        
         if serializer.is_valid():
-            
             sender_id = get_authenticated_user_id(request.COOKIES.get('jwt'))
-            
             receiver_id = serializer.validated_data['receiver_id']
-            
             chat = handle_chat_creation(sender_id, receiver_id, 'chat')
-            
-            chatSerializer = ChatSerializer(chat)
-            
-            return Response({"chat": chatSerializer.data})
-            
+            return Response({"chat": ChatSerializer(chat).data}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def get(self, request):
-        return Response({"contacts": "error"})
-    
-    
-    def get_serializer_class(self):
-        if self.action == 'create':
-            return StartChatSerializer
-        elif self.action == 'list' or self.action == 'retrieve':
-            return UserContactsSerializer
-        return super().get_serializer_class()
 
-
+    # get user contacts, groups, chats, people to meet
+    def get(self, request, *args, **kwargs):
+    
+        user_id = get_authenticated_user_id(request.COOKIES.get('jwt'))
         
+        contacts = get_user_contacts(user_id)
+        
+        serializer = UserContactsSerializer(data=contacts)
+        
+        return Response({"contacts": serializer.data}, status=status.HTTP_200_OK)
