88e7113d8bf8dbcd4beb6d94acfe69983ac56455
Agarvv <casluagarv@gmail.com>
2024-12-14 16:31:11 +0100

    redux can only have one store

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import chatReducer from './chatSlice';
+import chatReducer from './chat/chatSlice';
+import apiStatusReducer from './apiStatus/apiStatusSlice';
 
 const store = configureStore({
   reducer: {
-    chat: chatReducer,
+    chat: chatReducer, 
+    apiStatus: apiStatusReducer,
   },
 });
 
