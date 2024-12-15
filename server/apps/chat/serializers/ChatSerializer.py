d73797d3e25cc8881d58d1f0b9935062fccb0eab
Agarvv <casluagarv@gmail.com>
2024-12-15 14:51:59 +0100

    user to display info issue in server

--- server/apps/chat/serializers/ChatSerializer.py
+++ server/apps/chat/serializers/ChatSerializer.py
@@ -20,7 +20,10 @@ class ChatSerializer(serializers.ModelSerializer):
             sender_id = obj.sender_id
             receiver_id = obj.receiver_id
 
+        print("sender_id: ", sender_id, "receiver_id", receiver_id)
+
         other_user_id = receiver_id if sender_id == user_id else sender_id
+
         other_user = User.objects.get(id=other_user_id)
 
         return UserDetailsSerializer(other_user).data
