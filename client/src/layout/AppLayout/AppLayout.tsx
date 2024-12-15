f8a62bf07004b06738938987e6476bb2c76c6b64
Agarvv <casluagarv@gmail.com>
2024-12-15 18:32:39 +0100

    props types

--- client/src/layout/AppLayout/AppLayout.tsx
+++ client/src/layout/AppLayout/AppLayout.tsx
@@ -3,19 +3,23 @@ import Header from 'src/layout/header/Header';
 import ContactsAside from 'src/layout/contacts-aside/ContactsAside';
 import styles from './AppLayout.module.css';
 
-const AppLayout = ({ children }) => {
+interface AppLayoutProps {
+  children: React.ReactNode;
+}
+
+const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
   return (
-    <>
+    <div className={styles.layoutContainer}>
       <Header /> 
-      <div className={styles.layoutContainer}>
+      <div className={styles.layoutContent}>
         <div className={styles.layoutAside}>
           <ContactsAside /> 
         </div> 
         <div className={styles.layoutMain}>
-          <main>{ children }</main>
+          <main>{children}</main>
         </div> 
       </div>
-    </>
+    </div>
   );
 };
 
