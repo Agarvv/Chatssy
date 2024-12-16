212ad32011845153d76b4f43a46483b0532cf281
Agarvv <casluagarv@gmail.com>
2024-12-16 16:40:48 +0100

    correct endpoint

--- client/src/api/services/profile/ProfileService.ts
+++ client/src/api/services/profile/ProfileService.ts
@@ -1,7 +1,7 @@
 import axiosInstance from 'src/config/axiosConfig';
 
 export const getUserProfile = async (userId: number) => {
-  const response = await axiosInstance.get(`/user/${userId}/`)
+  const response = await axiosInstance.get(`/users/${userId}/`)
   return response.data.profile;
 }
 
