91d855c7d49eb810e7463ad3a58657312081c40f
Agarvv <casluagarv@gmail.com>
2024-12-16 17:47:23 +0100

    user can edit his info

--- client/src/features/profile/profile-username/ProfileUsername.tsx
+++ client/src/features/profile/profile-username/ProfileUsername.tsx
@@ -1,15 +1,53 @@
-import React from 'react'
-import { isSelfProps } from '../types'
-import styles from './ProfileUsername.module.css'
+import React, { useState } from 'react';
+import { isSelfProps } from '../types';
+import styles from './ProfileUsername.module.css';
 
 const ProfileUsername: React.FC<isSelfProps> = ({ isSelf }) => {
-  return (
-     <>
-      <div className={styles['ud-username']}>
-          <h4>Juan Pancracio</h4>
-      </div>
-     </>
-  )
-}
+  const [isEditing, setIsEditing] = useState(false);
+  const [tempUsername, setTempUsername] = useState('Juan Pancracio');
+  const [username, setUsername] = useState('Juan Pancracio');
 
-export default ProfileUsername
+  const handleUsernameClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleBlur = () => {
+    setTempUsername(username);
+    setIsEditing(false);
+  };
+
+  const handleSaveClick = () => {
+    setUsername(tempUsername);
+    setIsEditing(false);
+  };
+
+  const handleInputChange = (e: React.FormEvent<HTMLDivElement>) => {
+    setTempUsername(e.currentTarget.textContent || username);
+  };
+
+  return (
+    <>
+      <div className={styles['ud-username']}>
+        <h4
+          contentEditable={isEditing}
+          suppressContentEditableWarning
+          onClick={handleUsernameClick}
+          onInput={handleInputChange}
+          onBlur={handleBlur}
+          className={isEditing ? styles.editing : ''}
+        >
+          {tempUsername}
+        </h4>
+        <button
+          onClick={handleSaveClick}
+          className={styles['save-button']}
+          style={{ display: isEditing ? 'inline-block' : 'none' }}
+        >
+          Save
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default ProfileUsername;
