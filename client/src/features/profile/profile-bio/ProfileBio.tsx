2e1e8ca44120d7bf69309797de0bc94080bac7c7
Agarvv <casluagarv@gmail.com>
2024-12-16 18:20:40 +0100

    issues

--- client/src/features/profile/profile-bio/ProfileBio.tsx
+++ client/src/features/profile/profile-bio/ProfileBio.tsx
@@ -4,7 +4,9 @@ import styles from './ProfileBio.module.css'
 
 const ProfileBio: React.FC<isSelfProps> = ({ isSelf }) => {
     handleBioChange() {
-        
+        // just some kind of basic modal 
+        const bio = prompt('Enter your new bio')
+        alert('Your bio has been sabed')
     }
   return (
     <>
