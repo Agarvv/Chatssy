d682c8a71acd9b84af22becd1ae3af7369106956
Agarvv <casluagarv@gmail.com>
2024-12-16 17:22:47 +0100

    more import issues

--- client/src/features/profile/profile-banner/ProfileBanner.tsx
+++ client/src/features/profile/profile-banner/ProfileBanner.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { isSelfProps } from '../types'
 import styles from './ProfileBanner.module.css'
+import logo from 'src/logo.svg';
 
 const ProfileBanner: React.FC<isSelfProps> = ({ isSelf }) => {
   return (
--- client/src/features/profile/profile-picture/ProfilePicture.tsx
+++ client/src/features/profile/profile-picture/ProfilePicture.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { isSelfProps } from '../types'
 import styles from './ProfilePicture.module.css'
+import logo from 'src/logo.svg';
 
 const ProfilePicture: React.FC<isSelfProps> = ({ isSelf }) => {
   return (
