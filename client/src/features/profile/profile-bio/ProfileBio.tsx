3b38348986f10e97bb842a55bdc6038117a160ee
Agarvv <casluagarv@gmail.com>
2024-12-16 18:23:40 +0100

    issues

--- client/src/features/profile/profile-bio/ProfileBio.tsx
+++ client/src/features/profile/profile-bio/ProfileBio.tsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import { isSelfProps } from '../types'
-import styles from './ProfileBio.module.css'
+import React from 'react';
+import { isSelfProps } from '../types';
+import styles from './ProfileBio.module.css';
 
 const ProfileBio: React.FC<isSelfProps> = ({ isSelf }) => {
-    handleBioChange() {
-        // just some kind of basic modal 
-        const bio = prompt('Enter your new bio')
-        alert('Your bio has been sabed')
+  const handleBioChange = async () => {
+    const bio = prompt('Enter your new bio');
+    if (bio !== null) { 
+      alert('Your bio has been saved');
     }
+  };
+
   return (
     <>
-     <div onClick={handleBioChange} className={styles['ud-bio']}>
+      <div onClick={handleBioChange} className={styles['ud-bio']}>
         <p>Me Gusta el Helado De Fresa</p>
-     </div>
-    </> 
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default ProfileBio
+export default ProfileBio;
