e4ed2a9d6a9dc4f0aa0a03767dd0cb6f12cd05a2
Agarvv <casluagarv@gmail.com>
2024-12-17 19:30:37 +0100

    react audio upload

--- client/src/features/chatbox/ChatboxFooter/ChatboxFooterAudioButton/ChatboxFooterAudioButton.tsx
+++ client/src/features/chatbox/ChatboxFooter/ChatboxFooterAudioButton/ChatboxFooterAudioButton.tsx
@@ -3,7 +3,6 @@ import { useSelector } from 'react-redux';
 import { RootState } from 'src/store';
 import { useMessage } from 'src/hooks/useMessage';
 import { MediaRecorder } from 'react-media-recorder';
-import useImageUpload from 'src/hooks/useImageUpload';
 
 import styles from './ChatboxFooterAudioButton.module.css';
 import sharedMediaButtonStyles from '../ChatboxFooter.module.css';
@@ -27,20 +26,20 @@ const ChatboxFooterAudioButton: React.FC = () => {
     const file = new File([audioBlob], 'audio_recording.wav', { type: 'audio/wav' });
     setAudioFile(file);
     
-        try {
-           await uploadImage(file, 'audio');
-           console.log('video uploaded successfully!', imageUrl);
-          const message = {
-             'type': 'audio',
-             'value': imageUrl,
-             'identifier': chat.id,
-             'receiver_id': chat.user_to_display_info.id 
-          }
-          console.log('Final message object', message)
-          emitMessage(message)
-          } catch (error) {
-           console.error('Error uploading audio:', error);
-         }
+    try {
+        await uploadImage(file, 'audio');
+        console.log('audio uploaded successfully!', imageUrl);
+                const message = {
+                  'type': 'audio',
+                  'value': imageUrl,
+                  'identifier': chat.id,
+                  'receiver_id': chat.user_to_display_info.id 
+                }
+                console.log('Final message object', message)
+                emitMessage(message)
+            } catch (error) {
+                console.error('Error uploading video:', error);
+            }
 
   };
 
