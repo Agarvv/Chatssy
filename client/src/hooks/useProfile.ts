ff36c3b7d8ba435d547097aafeefb321b25ffc45
Agarvv <casluagarv@gmail.com>
2024-12-18 00:32:41 +0100

    issues

--- client/src/hooks/useProfile.ts
+++ client/src/hooks/useProfile.ts
@@ -27,7 +27,7 @@ const useProfile = ({ id }: Props) => {
    }
 
    const handleMutation = (mutationFn: Function, successMessage: string, errorMessage: string) => {
-      return useMutation({
+      const mutation = useMutation({
          mutationFn,
          onSuccess: () => dispatch(setSuccess(successMessage)),
          onError: (error: any) => {
@@ -36,12 +36,12 @@ const useProfile = ({ id }: Props) => {
          },
          onSettled: () => dispatch(setLoading())
       });
+
+      return mutation;
    };
 
    const setUserPicture = (url: string) => handleMutation(() => setUserProfilePicture(url), 'Your Profile Picture Has Been Updated !', 'Your Profile picture could not be updated...');
-   
    const setUserBio = (bio: string) => handleMutation(() => setUserProfileBio(bio), 'Your Profile BIO Has Been Updated !', 'Your Profile BIO could not be updated...');
-   
    const setUserBanner = (url: string) => handleMutation(() => setUserProfileBanner(url), 'Your Profile Banner Has Been Updated !', 'Your Profile Banner could not be updated...');
 
    return { profile: data, setUserPicture, setUserBio, setUserBanner };
