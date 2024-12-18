6c66b43ceb7c5c68fc2ee7509ac2bdfb06438338
Agarvv <casluagarv@gmail.com>
2024-12-18 17:19:29 +0100

    audio recording issues

--- client/src/features/chatbox/ChatboxFooter/ChatboxFooterAudioButton/ChatboxFooterAudioButton.tsx
+++ client/src/features/chatbox/ChatboxFooter/ChatboxFooterAudioButton/ChatboxFooterAudioButton.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from 'src/store';
 import { useMessage } from 'src/hooks/useMessage';
@@ -14,67 +14,70 @@ const ChatboxFooterAudioButton: React.FC = () => {
   const [audioFile, setAudioFile] = useState<File | null>(null);
   const { imageUrl, uploadImage } = useImageUpload();
 
-  let mediaRecorder: MediaRecorder | null = null;
-  let audioChunks: Blob[] = [];
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+  const audioChunksRef = useRef<Blob[]>([]);
 
   const startRecording = async () => {
-  setIsRecording(true);
+    setIsRecording(true);
 
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    mediaRecorder = new MediaRecorder(stream);
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      const mediaRecorder = new MediaRecorder(stream);
 
-    mediaRecorder.ondataavailable = (e) => {
-      if (e.data.size > 0) {
-        audioChunks.push(e.data);
-      }
-    };
-
-    mediaRecorder.onstop = async () => {
-      const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
-      const file = new File([audioBlob], 'audio_recording.wav', { type: 'audio/wav' });
-      setAudioFile(file);
-
-      if (file) {
-        try {
-          await uploadImage(file, 'audio');
-          console.log('audio uploaded successfully!', imageUrl);
-          const message = {
-            type: 'audio',
-            value: imageUrl,
-            identifier: chat?.id,
-            receiver_id: chat?.user_to_display_info.id 
-          };
-          console.log('Final message object', message);
-          emitMessage(message);
-        } catch (error) {
-          console.error('Error uploading Aud:', error);
+      mediaRecorder.ondataavailable = (e) => {
+        if (e.data.size > 0) {
+          audioChunksRef.current.push(e.data);
         }
-      }
+      };
 
-      audioChunks = [];
-    };
+      mediaRecorder.onstop = async () => {
+        const audioBlob = new Blob(audioChunksRef.current, { type: 'audio/wav' });
+        const file = new File([audioBlob], 'audio_recording.wav', { type: 'audio/wav' });
+        setAudioFile(file);
 
-    mediaRecorder.start();
-  } catch (err) {
-    console.error(err);
-  }
-};
+        if (file) {
+          try {
+            await uploadImage(file, 'audio');
+            console.log('audio uploaded successfully!', imageUrl);
+            const message = {
+              type: 'audio',
+              value: imageUrl,
+              identifier: chat?.id,
+              receiver_id: chat?.user_to_display_info.id,
+            };
+            console.log('Final message object', message);
+            emitMessage(message);
+          } catch (error) {
+            console.error('Error uploading Aud:', error);
+          }
+        }
+
+        audioChunksRef.current = [];
+      };
+
+      mediaRecorderRef.current = mediaRecorder;
+      mediaRecorder.start();
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   const stopRecording = () => {
-  if (mediaRecorder) {
-    console.log(`MediaRecorder state: ${mediaRecorder.state}`);
-    if (mediaRecorder.state !== 'inactive') {
-      mediaRecorder.stop();
-      setIsRecording(false);
+    const mediaRecorder = mediaRecorderRef.current;
+
+    if (mediaRecorder) {
+      console.log(`MediaRecorder state: ${mediaRecorder.state}`);
+      if (mediaRecorder.state !== 'inactive') {
+        mediaRecorder.stop();
+        setIsRecording(false);
+      } else {
+        alert('could not stop audio');
+      }
     } else {
-      alert('could not stop audio');
+      alert('no audio recorder.');
     }
-  } else {
-    alert('no audio recorder.');
-  }
-};
-  
+  };
+
   return (
     <>
       <div className={`${sharedMediaButtonStyles.fMediaButton} ${styles.fmbAudio}`}>
