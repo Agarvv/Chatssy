95336d9d767b2af270147d786ef52106466eacbd
Agarvv <casluagarv@gmail.com>
2024-12-18 13:36:14 +0100

    forgot to credentials include

--- client/src/api/services/profile/ProfileService.ts
+++ client/src/api/services/profile/ProfileService.ts
@@ -1,23 +1,33 @@
 import axiosInstance from 'src/config/axiosConfig';
 
 export const getUserProfile = async (userId: number) => {
-  const response = await axiosInstance.get(`/users/${userId}/`)
-  return response.data.profile;
-}
+    const response = await axiosInstance.get(`/users/${userId}/`);
+    return response.data.profile;
+};
 
 export const setUserProfilePicture = async (url: string) => {
-    const response = await axiosInstance.post('/users/update_profile_picture/', { picture: url })
-    return response
-}
+    const response = await axiosInstance.post(
+      '/users/update_profile_picture/', 
+      { picture: url }, 
+      { withCredentials: true }
+    );
+    return response.data;
+};
 
 export const setUserProfileBio = async (bio: string) => {
-    const response = await axiosInstance.post('/users/update_bio/', { bio: bio })
-    return response
-}
-
+    const response = await axiosInstance.post(
+      '/users/update_bio/', 
+      { bio }, 
+      { withCredentials: true }
+    );
+    return response.data;
+};
 
 export const setUserProfileBanner = async (url: string) => {
-    const response = await axiosInstance.post('/users/update_banner/', { banner: url })
-    return response
-}
-
+    const response = await axiosInstance.post(
+      '/users/update_banner/', 
+      { banner: url }, 
+      { withCredentials: true }
+    );
+    return response.data;
+};
